01145bb515a2aaa5338c49db11325b90e61059e2
Tonto <823498927@qq.com>
2020-05-18 17:30:52 +0800

    test docker

--- src/main/java/com/paladin/demo/controller/LoginController.java
+++ src/main/java/com/paladin/demo/controller/LoginController.java
@@ -128,6 +128,7 @@ public class LoginController {
     }
 
     @GetMapping(value = "/time")
+    @ResponseBody
     public String time() {
         return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
     }
