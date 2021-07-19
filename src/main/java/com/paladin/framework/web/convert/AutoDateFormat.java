fd915cd0101848c93339cc7035729d2a3ebcc907
Tonto <823498927@qq.com>
2021-07-19 14:09:02 +0800

    增加自动化json日期解析

---
+++ src/main/java/com/paladin/framework/web/convert/AutoDateFormat.java
@@ -0,0 +1,32 @@
+package com.paladin.framework.web.convert;
+
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * 根据日期字符串长度自动选择解析模式（简单高效的全局处理日期格式与带时分秒格式参数的解析）
+ */
+public class AutoDateFormat extends SimpleDateFormat {
+
+    private SimpleDateFormat shortDateFormat;
+
+    public AutoDateFormat() {
+        super("yyyy-MM-dd HH:mm:ss");
+        TimeZone timeZone = TimeZone.getTimeZone("GMT+8");
+        setTimeZone(timeZone);
+
+        shortDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        shortDateFormat.setTimeZone(timeZone);
+    }
+
+    @Override
+    public Date parse(String source, ParsePosition pos) {
+        if (source.length() > 10) {
+            return super.parse(source, pos);
+        } else {
+            return shortDateFormat.parse(source, pos);
+        }
+    }
+}
